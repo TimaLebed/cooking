3452e97152e2949c12fb6aa68a3c7fff7e2f040a
TimaLebed <timalebedevskiy@mail.ru>
2021-08-26 19:02:47 +0300

    feat: add header

---
+++ client/src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+
+function Button() {
+  return (
+    <button className="btn" type="button">Create cookBook</button>
+  )
+}
+
+export default Button;
---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+function Logo() {
+  return (
+    <div className="logo">
+      <div className="logo__img"></div>
+      <div className="logo__title">
+        <span>Feed</span>
+        <span>me</span>
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
